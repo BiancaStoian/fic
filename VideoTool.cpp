5d55b510d2d0e4af1456397fab81566b3bae7d39
BiancaStoian <32493452+BiancaStoian@users.noreply.github.com>
2017-10-19 08:49:16 +0300

    Update VideoTool.cpp

--- VideoTool.cpp
+++ VideoTool.cpp
@@ -197,7 +197,7 @@ int main(int argc, char* argv[])
 	//video capture object to acquire webcam feed
 	VideoCapture capture;
 	//open capture object at location zero (default location for webcam)
-	capture.open(0);
+	capture.open(0); //un ip ca string
 	//set height and width of capture frame
 	capture.set(CV_CAP_PROP_FRAME_WIDTH, FRAME_WIDTH);
 	capture.set(CV_CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT);
